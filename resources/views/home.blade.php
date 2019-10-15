48c52569f3a7d33acbda7fec5904872ca59a3e04
Deimys <deimiscamargo1022016@gmail.com>
2019-10-15 16:14:35 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="{{ asset('/js/jquery-3.4.1.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+    <title>Document</title>
+</head>
+<body>
+    <button type="submit" class="btn btn-danger">Hola Mundo</button>
+    <script src=""></script>
+
+    
+
+</body>
+</html>
